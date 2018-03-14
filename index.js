79c08b687d4dabaa133a1461e01125960b7ae1d9
Marilou Canon <marilou@dialexa.com>
2018-03-14 14:42:28 -0500

    updated index.js to start hapi server

--- index.js
+++ index.js
@@ -2,10 +2,18 @@
 
 const Hapi = require('hapi');
 
-// Creatre a server with a host and port
+// Create a server with a host and port
 // host and port are arbitrarily assigned?
-const server = new Hapi.Server();
-server.connection({
+const server = new Hapi.Server({
     host: 'localhost',
     port: 3000,
+});
+
+// Start the server
+server.start()
+.then(() => {
+    console.log('The server has started. You did it fam!');
+}).catch((err) => {
+    console.log('You dun fucked up Aaron', err);
+    throw err;
 });
